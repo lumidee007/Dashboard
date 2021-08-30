75934d23b3e0f93bad1e0923df194fdeec059056
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2021-08-30 11:01:00 +0100

    added seeting section

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-const getBgImage = async() => {
+let getElement = (id_class) => document.querySelector(id_class)
+let author = getElement('#author')
+let imageText = JSON.parse(localStorage.getItem("imageText"))
+let coinName = getElement('#coinName')
+let coinImage = getElement('#coinImg')
+let coinInfo = getElement('#coin-info')
+let time = getElement('#time')
+let greeting = getElement('#greeting')
+let weather = getElement('#weather')
+let settings = getElement('.setting')
+let tabSettings = getElement('.setting-tab')
+let cancelChange = getElement('.cancel-change')
+let saveChange = getElement('.save-change')
 
+
+imageText = !imageText ? localStorage.setItem("imageText", JSON.stringify('Nature')) : JSON.parse(localStorage.getItem("imageText"))
+
+
+const getBgImage = async(text) => {
     try {
-        const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+        const res = await fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${text}`)
         const data = await res.json()
         let image = data.urls.regular
-        console.log(image)
         document.body.style.backgroundImage = `url(${image})`
-        document.getElementById("author").textContent = `By: ${data.user.name}`
+        author.textContent = `Image by: ${data.user.name}`
 
     } catch (err) {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1426604966848-d7adac402bff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjUwMDg0MTQ&ixlib=rb-1.2.1&q=80&w=1080)`
-        document.getElementById("author").textContent = "By: Oluwafemi Midemi"
+        author.textContent = "By: Oluwafemi Midemi"
     }
 }
-getBgImage()
+getBgImage(imageText)
 
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+fetch("https://api.coingecko.com/api/v3/coins/solana")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
@@ -24,11 +40,11 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     })
     .then(data => {
 
-        document.getElementById("coinName").textContent = `${data.name}`
-        document.getElementById("coinImg").setAttribute("src", `${data.image.small}`)
+        coinName.textContent = `${data.name}`
+        coinImage.setAttribute("src", `${data.image.small}`)
 
-        document.getElementById("coin-info").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
+        coinInfo.innerHTML += `
+            <p>Price: $${data.market_data.current_price.usd}</p>
             <p>👆: $${data.market_data.high_24h.usd}</p>
             <p>👇: $${data.market_data.low_24h.usd}</p>
         `
@@ -38,14 +54,15 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
 
 function getCurrentTime() {
     const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", { timeStyle: "medium" })
+    time.textContent = date.toLocaleTimeString("en-us", { timeStyle: "medium" })
 }
 
 (function greetings() {
     let d = new Date();
     let n = d.getHours();
-    let greeting = n == 0 || n <= 11 ? "Good morning" : n == 12 || n <= 17 ? "Good afternoon" : n == 18 || n <= 23 ? "Good evening" : "Go to bed";
-    document.getElementById('greeting').textContent = greeting;
+    console.log("n = ", n)
+    let welcome = n == 0 || n <= 11 ? "Good morning" : n == 12 || n <= 17 ? "Good afternoon" : n == 18 || n <= 22 ? "Good evening" : "Go to bed";
+    greeting.textContent = welcome;
 })();
 
 setInterval(getCurrentTime, 1000)
@@ -68,7 +85,7 @@ navigator.geolocation.getCurrentPosition(pos => {
             let city = data.name
             let temp = Math.round(data.main.temp)
             let icon = data.weather[0].icon
-            document.getElementById("weather").innerHTML += `
+            weather.innerHTML += `
                     <img src=https://openweathermap.org/img/wn/${icon}@2x.png class="weather">
                     <p class="temp">${temp}º</p>
                     <p class="city">${city}</p>
@@ -76,3 +93,29 @@ navigator.geolocation.getCurrentPosition(pos => {
         })
         .catch(e => console.error(e))
 });
+
+
+settings.addEventListener('click', () => {
+    tabSettings.style.display = "flex"
+})
+
+cancelChange.addEventListener('click', () => {
+    tabSettings.style.display = "none"
+})
+
+
+function saveSetting() {
+    let inputText = document.getElementById('filter').value;
+    if (inputText) {
+        localStorage.setItem("imageText", JSON.stringify(inputText))
+        imageText = JSON.parse(localStorage.getItem("imageText"))
+        document.getElementById('filter').value = ''
+
+        getBgImage(imageText)
+    }
+    tabSettings.style.display = "none"
+}
+saveChange.addEventListener('click', saveSetting)
+
+
+document.addEventListener('keyup', (e) => e.key === 'Enter' ? saveSetting() : null)
